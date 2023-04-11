a4f342c235122256b9f06be7b0606f03d698141f
ImMiza <allan150199@outlook.fr>
2023-04-11 20:51:33 +0200

    rebasing

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import Vector from "./utils/Vector";
 import Entity from "./utils/Entity";
 import Spaceship from './utils/Spaceship';
 import Location from './utils/Location';
-import Location from "./utils/Location";
 
 function App() {
-
-  const [spaceship, setSpaceship] = useState(new Spaceship(new Location(50,50), new Vector(0,0), '', 40, 40, true, 10));
+  
+  const [spaceship, setSpaceship] = useState(new Spaceship(new Location(50,50), new Vector(0,0), '', 40, 40, true, 4));
   /**
    * The main function
    * call 30 times per seconds (30fps)
    */
   function main(): void {
-    console.log(spaceship);
-    setSpaceship(spaceship.startRotation());
+    setSpaceship(Object.create(spaceship.startRotation()));
   }
 
   /**
@@ -27,6 +25,7 @@ function App() {
 
     return () => clearInterval(interval);
   }, []);
+
   return <div>{spaceship.getJsxSpaceship()}</div>;
 }
 

9c24fc6cc7f952339112ea287aaf1e643d2d2566
ImMiza <allan150199@outlook.fr>
2023-04-11 15:25:31 +0200

    init thread main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
+  /**
+   * The main function
+   * call 30 times per seconds (30fps)
+   */
+  function main(): void {
+  }
+
+  /**
+   * don't touch
+   */
+  useEffect(() => {
+    const interval = setInterval(() => {
+      main();
+    }, 1000 / 30);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>Watch out !</div>;
 }
 
 export default App;

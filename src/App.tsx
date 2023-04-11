ce3a3f9eb02870d39f7aeae04490231cbddf6b2d
Guilloton <samuel.guilloton01@gmail.com>
2023-04-11 17:57:56 +0200

    create spaceship but there is bug yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Vector from "./utils/Vector";
 import Entity from "./utils/Entity";
+import Spaceship from './utils/Spaceship';
+import Location from './utils/Location';
 
 function App() {
-
+  
+  const [spaceship, setSpaceship] = useState(new Spaceship(new Location(50,50), new Vector(0,0), '', 40, 40, true, 10));
   /**
    * The main function
    * call 30 times per seconds (30fps)
    */
   function main(): void {
+    console.log(spaceship);
+    setSpaceship(spaceship.startRotation());
   }
 
   /**
@@ -21,8 +26,7 @@ function App() {
 
     return () => clearInterval(interval);
   }, []);
-
-  return <div>Watch out !</div>;
+  return <div>{spaceship.getJsxSpaceship()}</div>;
 }
 
 export default App;
---
+++ src/utils/Spaceship.tsx
@@ -0,0 +1,66 @@
+import Location from "./Location";
+import Vector from "./Vector";
+import Entity from "./Entity";
+import { number } from "yargs";
+
+export default class Spaceship extends Entity {
+    private isMoving: boolean;
+    private rotation: number;
+    private rotationSpeed: number;
+
+    constructor(location: Location, vector: Vector, image: string,  width: number, height: number, isMoving: boolean, rotationSpeed: number) {
+        super(location, vector, image, width, height);
+        this.isMoving = isMoving;
+        this.rotation = 0;
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    set setIsMoving(isMoving: boolean) {
+        this.isMoving = isMoving;
+    }
+
+    get getIsMoving(): boolean {
+        return this.isMoving;
+    }
+
+    public setRotation(rotation: number) {
+        this.rotation = rotation;
+    }
+
+    get getRotation(): number {
+        return this.rotation;
+    }
+
+    set setRotationSpeed(rotationSpeed: number) {
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    get getRotationSpeed(): number {
+        return this.rotationSpeed;
+    }
+
+    startRotation(): Spaceship {
+        const currentRotation = this.getRotation;
+        const newRotation = (currentRotation + this.rotationSpeed)%360;
+        this.setRotation(newRotation);
+        return this;
+    }
+
+    getJsxSpaceship():JSX.Element {
+    console.log(this.getIsMoving);
+    console.log(this.getWidth);
+    console.log(this.getWidth);
+        return (
+            <div style={{
+                width: `${this.getWidth}px`,
+                height: `${this.getHeight}px`,
+                backgroundColor: 'red',
+                position: 'fixed',
+                left: `${this.getLocation.getX}px`,
+                top:  `${this.getLocation.getY}px`,
+                transform: `rotate(${this.getRotation}deg)`,
+              }}>
+            </div>
+        )
+    }
+}

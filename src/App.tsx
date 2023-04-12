be5c363ec4f1698d7fa8ea7b2cf8f9dc16852922
Corentyn <48148602+Corentyn@users.noreply.github.com>
2023-04-12 15:06:05 +0200

    timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,19 @@ import React, { useEffect, useState } from "react";
 import Vector from "./utils/Vector";
 import Entity from "./utils/Entity";
 import Spaceship from "./utils/Spaceship";
-import Location from './utils/Location';
-import Timer, {timervalue} from './components/settings/timer';
+import Location from "./utils/Location";
+import Timer, { timervalue } from "./components/settings/timer";
 import Popup from "./components/popup/Popup";
 import Scoreboard from "./components/popup/Scoreboard";
 import Meteor from "./utils/Meteor";
 import MusicPlayer, { Music } from "./utils/MusicPlayer";
 import Modal from "./components/Modal";
 
-
-
 function App() {
-
   const [isStart, setStart] = React.useState<boolean>(false);
   const [isEnd, setEnd] = React.useState<boolean>(false);
   const [currentMusic, setCurrentMusic] = React.useState<Music>(Music.nomusic);
   const [isLoop, setLoop] = React.useState<boolean>(true);
-
 
   let timeBeforeApparition = 60;
   let currentTimeBefore = timeBeforeApparition;
@@ -72,15 +68,17 @@ function App() {
    * Update all the meteor positions and check collision
    */
   function updateMeteors(): void {
-    setMeteors((prev) => prev.map(m => {
-      if (Entity.checkCollision(m, spaceship)) {
-        setCurrentMusic(Music.explosion);
-        setEnd(true);
-        setStart(false);
-      }
-      m.move();
-      return m;
-    }));
+    setMeteors((prev) =>
+      prev.map((m) => {
+        if (Entity.checkCollision(m, spaceship)) {
+          setCurrentMusic(Music.explosion);
+          setEnd(true);
+          setStart(false);
+        }
+        m.move();
+        return m;
+      })
+    );
   }
 
   /**
@@ -150,71 +148,74 @@ function App() {
   }, [isStart, isEnd]);
 
   useEffect(() => {
-
     document.addEventListener("keydown", (event: KeyboardEvent) => {
       if (event.code === "Space") {
-         if(spaceship.getIsMoving === true){
-            spaceship.setIsMoving(false);
-            spaceship.displacement(spaceship.getLocation.getRotation , 30);
-            spaceship.screenLimit(spaceship.getLocation)
-            spaceship.setIsMoving(true);
-            return;
-         }
+        if (spaceship.getIsMoving === true) {
+          spaceship.setIsMoving(false);
+          spaceship.displacement(spaceship.getLocation.getRotation, 30);
+          spaceship.screenLimit(spaceship.getLocation);
+          spaceship.setIsMoving(true);
+          return;
+        }
       }
     });
-
   }, []);
 
   return (
-      <div>
-        <Timer timer_on={isStart} on_timer_end={(value) => setTimer(value)} />
-        {
-          !isStart &&
-            <div>
-              <div className="background">
+    <div>
+      <Timer timer_on={isStart} on_timer_end={(value) => setTimer(value)} />
+      {!isStart && (
+        <div>
+          <div className="background">
+            <div className="shape1"></div>
+            <div className="shape2"></div>
+          </div>
+          <form>
+            <div className="pen-intro">
+              <h1>Watch Out</h1>
+            </div>
+            <button className="eightbit-btn" onClick={() => setStart(true)}>
+              Play Game
+            </button>
+            <button className="eightbit-btn eightbit-btn--proceed">
+              Setting
+            </button>
+            <button className="eightbit-btn eightbit-btn--reset">
+              Information
+            </button>
+          </form>
+        </div>
+      )}
+      {isEnd && (
+        <>
+          <div>
+            <div className="background">
               <div className="shape1"></div>
               <div className="shape2"></div>
-              </div>
-              <form>
-                <div className="pen-intro">
-                  <h1>Watch Out</h1>
-                </div>
-                <button className="eightbit-btn" onClick={() => setStart(true)} >Play Game</button>
-                <button className="eightbit-btn eightbit-btn--proceed">Setting</button>
-                <button className="eightbit-btn eightbit-btn--reset">Information</button>
-              </form>
             </div>
-        }
-        {
-          isEnd &&
-            <>
-              <div>
-                <div className="background">
-                  <div className="shape1"></div>
-                  <div className="shape2"></div>
-                </div>
-                <form>
-                  <div className="pen-intro">
-                    <h1>Game Over</h1>
-                  </div>
-                  <button className="eightbit-btn eightbit-btn--proceed" onClick={() => retry()} >Try again</button>
-                  <a className="eightbit-btn eightbit-btn--reset">Return</a>
-                </form>
+            <form>
+              <div className="pen-intro">
+                <h1>Game Over</h1>
+                <p>
+                  Your Score : {timer?.minutes}min {timer?.seconds}s{" "}
+                  {timer?.milliseconds}ms
+                </p>
               </div>
-              <Scoreboard
-                  title={"Votre score : 1 min 30 sec"}
-                  buttonText={"Save Score"}
-                  buttonList={"List of score"}
-                  onClickSave={() => {
-                    setOpenModal(true);
-                  }}
-              />
-            </>
-        }
-        {spaceship.getJsxSpaceship()}
-        {meteors.map(m => m.jsxElement)}
-        <MusicPlayer music={currentMusic} loop={false} />
-      </div>
+              <button
+                className="eightbit-btn eightbit-btn--proceed"
+                onClick={() => retry()}
+              >
+                Try again
+              </button>
+              <a className="eightbit-btn eightbit-btn--reset">Return</a>
+            </form>
+          </div>
+        </>
+      )}
+      {spaceship.getJsxSpaceship()}
+      {meteors.map((m) => m.jsxElement)}
+      <MusicPlayer music={currentMusic} loop={false} />
+    </div>
   );
 }
 
